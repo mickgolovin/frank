3eef66324eb8336f6d309736851f4de23d234d06
Mick Golovin <mick.golovin@gmail.com>
2019-05-02 22:27:14 +0300

    custom.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ const options = {
 const app = express()
 const php_serv = php_fpm(options);
 app.use(php_serv);
-app.use(express.static('php_files'));
+//app.use(express.static('php_files'));
 
 app.listen(3000, '0.0.0.0')
